4078786ab86b073dfc6b3f132e17f3f98d697f42
Zeenat212 <24ntucsfl1219@student.ntu.edu.pk>
2025-03-04 00:47:04 -0800

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Objects_2_
+{
+    internal class Employee
+    {
+        public int EmployeeID;
+        public string Name;
+        public string Department;
+        public double Salary;
+
+        public Employee(int id,string name,string department,double salary)
+        {
+            EmployeeID = id;
+            Name = name;
+            Department = department;
+            Salary = salary;
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Employee Details are : ");
+            Console.WriteLine($"Employee ID is {EmployeeID}");
+            Console.WriteLine($"Employee Name is {Name}");
+            Console.WriteLine($"Employee Department is {Department}");
+            Console.WriteLine($"Employee Salary is {Salary}");
+        }
+      public void IncreaseSalary(double amount)
+        {
+           if(amount > 0)
+            {
+                Salary += amount;
+                Console.WriteLine($"Salary increased is {Salary}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Salary Increases Amount");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,21 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Classes_and_Objects
+namespace Classes_and_Objects_2_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
-            book1.Title = "Programming";
-            book1.Author = "Waqar";
-            book1.ISBN = "2335637";
-            book1.Price = 20.0;
-            book1.DisplayBookInfo();
-            book1.ApplyDiscount(10);
-            Console.Write("Updated Book Details: ");
-            book1.DisplayBookInfo();
+            Employee employee = new Employee(11, "Saleem", "Textile", 20000.0);
+            employee.DisplayDetails();
+            employee.IncreaseSalary(10000);
+            employee.DisplayDetails();
+            //My name is zeenat
         }
     }
 }
