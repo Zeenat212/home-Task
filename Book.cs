1b7eabb66694672fc2ea3a136c87416a55533b48
Zeenat212 <24ntucsfl1219@student.ntu.edu.pk>
2025-03-04 11:05:10 -0800

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Objects
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public string ISBN;
+        public double Price;
+
+
+        public void DisplayBookInfo()
+        {
+            Console.WriteLine("Book Info is :");
+            Console.WriteLine($"Book Title is {Title}");
+            Console.WriteLine($"Book Author is {Author}");
+            Console.WriteLine($"Book ISBN is {ISBN}");
+            Console.WriteLine($"Book Price is {Price}");
+        }
+        public void ApplyDiscount(double percentage)
+        {
+             if(percentage >= 0 && percentage <= 100)
+            {
+                Price=Price-(Price*percentage/100);
+                Console.WriteLine($"Discount Applied is {Price} ");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Discount Percentage");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.Title = "Programming";
+            book1.Author = "Waqar";
+            book1.ISBN = "2335637";
+            book1.Price = 20.0;
+            book1.DisplayBookInfo();
+            book1.ApplyDiscount(10);
+            Console.Write("Updated Book Details: ");
+            book1.DisplayBookInfo();
+        }
+    }
+}
